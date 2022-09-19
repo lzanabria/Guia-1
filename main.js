9961482c387a6a545245c9ff3a48855892056bf0
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-09-19 10:35:28 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let n1, n2, n3, n4, media;
-        n1=Number(prompt("Ingrese el valor del número 1: "));
-        n2=Number(prompt("Ingrese el valor del número 2: "));
-        n3=Number(prompt("Ingrese el valor del número 3: "));
-        n4=Number(prompt("Ingrese el valor del número 4: "));
-        media=(n1+n2+n3+n4)/4;
-        alert("La media de los números es: "+media);
+        let base, altura, area;
+        base = prompt("Ingrese la base del triángulo: ");
+        altura = prompt("Ingrese la altura del triángulo: ");
+        area = (base * altura) / 2;
+        alert("El área del triángulo es: "+area);
     }, 500);
 });
