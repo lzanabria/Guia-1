2ec81d196bfd4d1b85fe37651811427d53a7fb9a
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-09-19 12:21:39 -0500

    Ejercicio 18

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let porc_iva, val_prod, val_iva, val_total;
-        porc_iva = Number(prompt("Ingrese el porcentaje de IVA (sólo número): "));
-        val_prod = Number(prompt("Ingrese el valor del producto : $ "));
-        val_iva = val_prod * porc_iva / 100;
-        val_total = val_prod + val_iva;
-        alert("El valor del IVA de este producto es de: $"+val_iva);
-        alert("El valor total del producto es de: $"+val_total);
+        let nom_ape, h_trab, sueldo;
+        const pago_h=15300;
+
+        nom_ape = prompt("Ingrese su nombre y apellido: ");
+        h_trab=Number(prompt("Ingrese las horas trabajadas: "));
+        sueldo=h_trab*pago_h;
+        alert("Empleado: "+nom_ape
+            +"\nHoras trabjadas: "+h_trab
+            +"\nValor pago por hora: $"+pago_h
+            +"\nSueldo: $"+sueldo);
     }, 500);
-});
+}); 
