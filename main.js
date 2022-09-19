b287db7ecce82535e7164f4dfd13b3188724f08b
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-09-19 12:07:46 -0500

    Ejercicio 16

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let grad_c, conv;
-        grad_c=Number(prompt("Ingrese el valor de la temperatura en °C: "));
-        conv=(grad_c*9/5)+32;
-        alert("La temperatura convertida en °F es: "+conv);
+        let nota1, nota2, nota3, exam_fin, trab_fin, prom_parc, prom_examf, prom_trabf, promf;
+        
+        nota1 = Number(prompt("Ingrese la nota del parcial 1: "));
+        nota2 = Number(prompt("Ingrese la nota del parcial 2: "));
+        nota3 = Number(prompt("Ingrese la nota del parcial 3: "));
+        exam_fin = Number(prompt("Ingrese la nota del examen final: "));
+        trab_fin = Number(prompt("Ingrese la nota del trabajo final: "));
+
+        prom_parc = ((nota1 + nota2 + nota3) / 3) * 0.55;
+        prom_examf = exam_fin * 0.30;
+        prom_trabf = trab_fin * 0.15;
+        promf = prom_parc + prom_examf + prom_trabf;
+
+        alert("El promedio final es: " + promf);
     }, 500);
 });
